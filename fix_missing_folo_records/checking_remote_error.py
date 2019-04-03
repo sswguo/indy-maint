6557ffc98e09d889492029269e178546d68bd8cd
Wenjie Guo <wguo@redhat.com>
2019-04-03 16:31:59 +0800

    init fix missing folo records

---
+++ fix_missing_folo_records/checking_remote_error.py
@@ -0,0 +1,44 @@
+import json
+import os
+
+# picking the path up if the storeKey is "maven:remote:", see following examples:
+# 
+# {
+#    "storeKey" : "maven:remote:",
+#    "accessChannel" : "MAVEN_REPO",
+#    "path" : "/org/jboss/shrinkwrap/descriptors/shrinkwrap-descriptors-api-base/2.0.0.redhat-2/shrinkwrap-descriptors-api-base-2.0.0.redhat-2.jar",
+#    "originUrl" : "",
+#    "localUrl" : "http://indy.psi.redhat.com/api/content/maven/remote//org/jboss/shrinkwrap/descriptors/shrinkwrap-descriptors-api-base/2.0.0.redhat-2/shrinkwrap-descriptors-api-base-2.0.0.redhat-2.jar",
+#    "md5" : "b6d3b560984866dac9d171ead2020692",
+#    "sha256" : "927df9d739261aeac4f1f1dcb90fc64165e01fd113552e92d052062560eed5d6",
+#    "sha1" : "c5328b6680f161fb007dab7315036a079e873899",
+#    "size" : 17614
+# }
+
+DIR="./data/"
+
+count = 0
+array = []
+subarr = []
+for file in os.listdir(DIR):
+  if file.endswith('.json'):
+    with open(DIR+file, 'r+') as f:
+      data = json.load(f)
+      for download in data['downloads']:
+        if ( "maven:remote:" == download['storeKey'] ):
+          print download['localUrl']
+          count = count + 1
+          path = download['path']
+          subparent = path[0:path.rfind('/')]
+          if ( path not in array ):
+            array.append(path)
+          if ( subparent not in subarr ):
+            subarr.append(subparent)
+
+print count
+print len(array)
+print len(subarr)
+
+#for sub in subarr:
+#  print sub    
+
---
+++ fix_missing_folo_records/replace_accesschannel.sh
@@ -0,0 +1,1 @@
+sed -i .bak 's/"accessChannel" : "MAVEN"/"accessChannel" : "MAVEN_REPO"/g' *.json
